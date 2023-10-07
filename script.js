284094e71a90a0c08d6732b9304d8afecb44e076
Ankur Tyagi <ankurtyagi7505@gmail.com>
2023-10-07 15:06:06 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const Color = document.querySelector('#color')
 const Reset = document.querySelector('.reset')
-// console.log(btn1)
 
 const fun = () => {
-    // let RandomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let RandomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + RandomColor;
     Color.innerHTML = "#" + RandomColor;
     Color.style.display = 'block';
 }
